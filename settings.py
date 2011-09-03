2ca7cf04580c3e16bf182f0eec89cca8cdf62f9f
Wesley Childs <childs.wesley@gmail.com>
2011-09-03 10:28:06 +0100

    Defined static and templates directory

--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
 # Django settings for djangobase project.
+import os
+
+DIRNAME = os.path.dirname(__file__)
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -69,9 +73,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    (os.path.join(DIRNAME, "static")),
 )
 
 # List of finder classes that know how to find static files in
@@ -103,9 +105,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'djangobase.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    (os.path.join(DIRNAME, "templates")),
 )
 
 INSTALLED_APPS = (
