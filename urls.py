b4a0edd639621ce7d369f12b25121e34fb9f03bd
Wesley Childs <childs.wesley@gmail.com>
2011-09-07 21:11:56 +0100

    Added robots and humans

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
 from bootstrap.views import home, inside, ajax_example
 
@@ -10,9 +11,13 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     
     #Registration
-    (r'^accounts/', include('registration.backends.simple.urls')),
+    (r'^accounts/', include('registration.urls')),
     
     (r'^$', home),
     (r'^inside/$', inside),
-    (r'^ajax_example', ajax_example)
+    (r'^ajax_example', ajax_example),
+    
+    #Humans and Robots
+    ('^humans.txt$', direct_to_template, {'template':'humans.txt', 'mimetype':'text/plain'}),
+    ('^robots.txt$', direct_to_template, {'template':'robots.txt', 'mimetype':'text/plain'}),
 )
